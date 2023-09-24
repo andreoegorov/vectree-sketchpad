85f21fa5ed91fe29496f6d623f5eb66f97c62cf4
Andrey <andreo.egorov@mail.ru>
2023-09-25 00:08:14 +0400

    create display grid

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+const display = document.getElementById("display");
+const defaultDisplaySize = 16;
+const displaySize = {
+    height: 400,
+    width: 400
+}
+
+function createGrid(size=defaultDisplaySize) {
+    let div = document.createElement("div");
+        div.style.width = '97.5px';
+        div.style.height = '97.5px';
+        div.style.float = 'left';
+        div.style.backgroundColor = 'red';
+    for (let i = 0; i < size; i++) {
+        display.appendChild(div.cloneNode());
+    }
+}
+
+function resetGrid() {
+    
+}
+createGrid();
+
